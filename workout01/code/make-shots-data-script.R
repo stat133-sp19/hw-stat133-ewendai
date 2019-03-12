a5cf11ad1aecab86541857e466182b1dcb3b3d2d
ewendai <yiwendai99@berkeley.edu>
2019-03-11 21:21:37 -0700

    Commit make-shots-data-script.R before sinking.

--- workout01/code/make-shots-data-script.R
+++ workout01/code/make-shots-data-script.R
@@ -19,3 +19,22 @@ thompson['name'] = 'Klay Thompson'
 curry['name'] = 'Stephen Curry'
 
 # Change values of shot_made_flag to be more descriptive
+iguodala[iguodala['shot_made_flag'] == 'y', 'shot_made_flag'] = 'shot_yes'
+iguodala[iguodala['shot_made_flag'] == 'n', 'shot_made_flag'] = 'shot_no'
+green[green['shot_made_flag'] == 'y', 'shot_made_flag'] = 'shot_yes'
+green[green['shot_made_flag'] == 'n', 'shot_made_flag'] = 'shot_no'
+durant[durant['shot_made_flag'] == 'y', 'shot_made_flag'] = 'shot_yes'
+durant[durant['shot_made_flag'] == 'n', 'shot_made_flag'] = 'shot_no'
+thompson[thompson['shot_made_flag'] == 'y', 'shot_made_flag'] = 'shot_yes'
+thompson[thompson['shot_made_flag'] == 'n', 'shot_made_flag'] = 'shot_no'
+curry[curry['shot_made_flag'] == 'y', 'shot_made_flag'] = 'shot_yes'
+curry[curry['shot_made_flag'] == 'n', 'shot_made_flag'] = 'shot_no'
+
+# Add column minute
+iguodala['minute'] = iguodala['period']*4 - iguodala['minutes_remaining']
+green['minute'] = green['period']*4 - green['minutes_remaining']
+durant['minute'] = durant['period']*4 - durant['minutes_remaining']
+thompson['minute'] = thompson['period']*4 - thompson['minutes_remaining']
+curry['minute'] = curry['period']*4 - curry['minutes_remaining']
+
+# Sinking
